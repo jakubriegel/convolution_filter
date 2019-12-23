aae98707651efb3f2aa6233729f6745d11518701
jakubriegel <jakubriegel112@gmail.com>
2019-12-23 18:09:52 +0100

    partial result stored in current_gen/next_gen

--- convolutionfilter/worker.py
+++ convolutionfilter/worker.py
@@ -13,6 +13,10 @@ class _WorkerResult:
     def set_row(self, y: int, value: np.ndarray):
         self._data[y] = value
 
+    def copy_from(self, source: np.ndarray) -> None:
+        for i in range(0, len(source)):
+            self.set_row(i, source[i])
+
     def get(self): return self._data
 
 
@@ -29,31 +33,35 @@ _WorkersManager.register('result', _WorkerResult)
 class _ConvWorker(Process):
 
     def __init__(
-            self,
-            n: int, data: np.ndarray,
-            matrix: List[List[int]], result: _WorkerResult
+            self, n: int, data: np.ndarray, matrix: List[List[int]], result: _WorkerResult
     ) -> None:
         super().__init__(name=f'worker {n}')
         self._data = data
         self._matrix = matrix
         self._result = result
 
-        self._row_len = len(data[0])
+        self._height = len(data)
+        self.width = len(data[0])
+        self._current_gen = self._data
+        self._next_gen = self._empty_gen()
+
+    def _empty_gen(self): return np.empty((self._height, self.width, 3), dtype=np.uint8)
 
     def run(self) -> None:
         self._process_rows()
+        self._result.copy_from(self._next_gen)
 
     def _process_rows(self) -> None:
-        new_row = self._process_row(self._data[0:1])
-        self._result.set_row(0, new_row)
-        for i in range(1, len(self._data)):
-            new_row = self._process_row(self._data[i-1:i+2])
-            self._result.set_row(i, new_row)
+        new_row = self._process_row(self._current_gen[0:1])
+        self._next_gen[0] = new_row
+        for i in range(1, len(self._current_gen)):
+            new_row = self._process_row(self._current_gen[i-1:i+2])
+            self._next_gen[i] = new_row
 
     def _process_row(self, row: np.ndarray) -> np.ndarray:
-        new_row = np.empty((self._row_len, 3), dtype=np.uint8)
+        new_row = np.empty((self.width, 3), dtype=np.uint8)
         new_row[0] = self._process_pixel(row[0:4, 0:1])
-        for i in range(1, self._row_len):
+        for i in range(1, self.width):
             new_row[i] = self._process_pixel(row[0:4, i-1:i+2])
         return new_row
 
